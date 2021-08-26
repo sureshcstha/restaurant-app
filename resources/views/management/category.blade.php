35d0944330e72d816812385fbcb3f5fa6d4cfd13
Suresh Shrestha <sureshcstha@gmail.com>
2021-08-25 21:25:39 -0600

    create a category - controller

--- resources/views/management/category.blade.php
+++ resources/views/management/category.blade.php
@@ -11,7 +11,13 @@
                 <a href="/management/category" class="list-group-item list-group-item-action"><i class="fas fa-users-cog"></i> User</a>
             </div>
         </div>
+
         <div class="col-md-8">
+            <i class="fas fa-align-justify"></i> Category
+            <a href="/management/category/create" class="btn btn-success btn-sm float-right"><i class="fas fa-plus"></i> Create Category</a>
+            <hr>
+
+
         </div>
     </div>
 </div>
--- resources/views/management/createCategory.blade.php
+++ resources/views/management/createCategory.blade.php
@@ -11,7 +11,21 @@
                 <a href="/management/category" class="list-group-item list-group-item-action"><i class="fas fa-users-cog"></i> User</a>
             </div>
         </div>
+
         <div class="col-md-8">
+            <i class="fas fa-align-justify"></i> Create a Category
+            <hr>
+
+            <form action="/management/category" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="categoryName">Category Name</label>
+                    <input type="text" name="name" class="form-control" placeholder="category...">
+                </div>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+
+
         </div>
     </div>
 </div>
