19133a2a16cf324f40ce7a7d10d86ffd57406a28
Suresh Shrestha <sureshcstha@gmail.com>
2021-08-25 20:13:37 -0600

    create and design the main function page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Main Functions</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,28 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <div class="container">
+                        <div class="row text-center">
+                            <div class="col-sm-4">
+                                <a href="/management">
+                                    <h4>Management</h4>
+                                    <img width="50px" src="{{asset('images/web-management.svg')}}"/>
+                                </a>
+                            </div>
+                            <div class="col-sm-4">
+                                <a href="/cashier">
+                                    <h4>Cashier</h4>
+                                    <img width="50px" src="{{asset('images/cashier-machine.svg')}}"/>
+                                </a>
+                            </div>
+                            <div class="col-sm-4">
+                                <a href="/report">
+                                    <h4>Report</h4>
+                                    <img width="50px" src="{{asset('images/report.svg')}}"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
