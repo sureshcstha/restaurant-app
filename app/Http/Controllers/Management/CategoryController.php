98789fcf2c741814dce78eb645ec5ac8817f657c
Suresh Shrestha <sureshcstha@gmail.com>
2021-08-26 00:33:21 -0600

    update a category

--- app/Http/Controllers/Management/CategoryController.php
+++ app/Http/Controllers/Management/CategoryController.php
@@ -67,7 +67,8 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $category = Category::find($id);
+        return view('management.editCategory')->with('category', $category);
     }
 
     /**
@@ -79,7 +80,15 @@ class CategoryController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'name' => 'required|unique:categories|max:255'
+        ]);
+
+        $category = Category::find($id);
+        $category->name = $request->name;
+        $category->save();
+        $request->session()->flash('status', $request->name. " is updated successfully.");
+        return(redirect('/management/category'));
     }
 
     /**
