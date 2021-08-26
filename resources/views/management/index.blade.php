b13b077c19bcf33389bd86d22199b5f918dde77e
Suresh Shrestha <sureshcstha@gmail.com>
2021-08-25 20:26:06 -0600

    create and design the data management page

---
+++ resources/views/management/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <div class="list-group">
+                <a href="management/category" class="list-group-item list-group-item-action"><i class="fas fa-align-justify"></i> Category</a>
+                <a href="management/category" class="list-group-item list-group-item-action"><i class="fas fa-hamburger"></i> Menu</a>
+                <a href="management/category" class="list-group-item list-group-item-action"><i class="fas fa-chair"></i> Table</a>
+                <a href="management/category" class="list-group-item list-group-item-action"><i class="fas fa-users-cog"></i> User</a>
+            </div>
+        </div>
+        <div class="col-md-8">
+
+        </div>
+    </div>
+</div>
+@endsection
