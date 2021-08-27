0f7a96bc17a55f5e2b81b0da4bc28ecd5a103140
Suresh Shrestha <sureshcstha@gmail.com>
2021-08-26 22:24:56 -0600

    create a menu - design a form

---
+++ resources/views/management/createMenu.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <div class="list-group">
+                <a href="/management/category" class="list-group-item list-group-item-action"><i class="fas fa-align-justify"></i> Category</a>
+                <a href="/management/category" class="list-group-item list-group-item-action"><i class="fas fa-hamburger"></i> Menu</a>
+                <a href="/management/category" class="list-group-item list-group-item-action"><i class="fas fa-chair"></i> Table</a>
+                <a href="/management/category" class="list-group-item list-group-item-action"><i class="fas fa-users-cog"></i> User</a>
+            </div>
+        </div>
+
+        <div class="col-md-8">
+            <i class="fas fa-hamburger"></i> Create a Menu
+            <hr>
+            @if($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <form action="/management/menu" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="menuName">Menu Name</label>
+                    <input type="text" name="name" class="form-control" placeholder="Menu...">
+                </div>
+
+                <label for="menuPrice">Price</label>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">$</span>
+                    </div>
+                    <input type="text" name="price" class="form-control" arial-label="Amount (to the nearest dollor)">
+                    <div class="input-group-append">
+                        <span class="input-group-text">.00</span>
+                    </div>
+                </div>
+
+                <label for="menuImage">Image</label>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">Upload</span>
+                    </div>
+                    <div class="custom-file">
+                        <input type="file" name="image" class="custom-file-input" id="inputGroupFile01" arial-label="Amount (to the nearest dollor)">
+                        <label for="inputGroupFile01" class="custom-file-label">Choose File</label>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <input type="text" name="description" class="form-control" placeholder="Description...">
+                </div>
+                <div class="form-group">
+                    <label for="category">Category</label>
+                    <select name="category_id" class="form-control">
+                        @foreach ($categories as $category)
+                            <option value="{{$category->id}}">{{$category->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+
+
+        </div>
+    </div>
+</div>
+@endsection
