e44cecc787b4bb1f54e6ebdad9b64ce3d757da7f
Suresh Shrestha <sureshcstha@gmail.com>
2021-11-02 22:44:57 -0600

    Read and display all tables in the cashier page

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::get('/cashier', function () {
     return view('cashier.index');
 });
 
+Route::get('/cashier/getTable', 'Cashier\CashierController@getTables');
+
 Route::resource('management/category', 'Management\CategoryController');
 
 Route::resource('management/menu', 'Management\MenuController');
