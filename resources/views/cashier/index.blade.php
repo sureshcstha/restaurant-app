b31044ba42aafadac9f54afecdc1d3e81d5f5212
Suresh Shrestha <sureshcstha@gmail.com>
2021-11-13 11:21:07 -0700

    Payment Dialogue Box

--- resources/views/cashier/index.blade.php
+++ resources/views/cashier/index.blade.php
@@ -24,6 +24,27 @@
     </div>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Payment</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h3 class="totalAmount"></h3>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <script>
 $(document).ready(function(){
     // make table-detail hidden by default
@@ -115,6 +136,12 @@ $("#order-detail").on("click", ".btn-delete-saledetail",function(){
             $("#order-detail").html(data);
         }
     })
+});
+
+// when a user click on the payment button
+$("#order-detail").on("click", ".btn-payment", function(){
+    var totalAmout = $(this).attr('data-totalAmount');
+    $(".totalAmount").html("Total Amount " + totalAmout);
 });
 
 });
