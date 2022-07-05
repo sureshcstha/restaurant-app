481f381c1e1e7e5854a828de01d1812ee5dbe475
Suresh Shrestha <sureshshrestha@mail.weber.edu>
2022-07-05 11:41:40 -0600

    Using Middleware to Prevent Unauthenticated and Unauthorized Users

--- routes/web.php
+++ routes/web.php
@@ -19,31 +19,35 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/management', function () {
-    return view('management.index');
+Route::middleware(['auth'])->group(function(){
+    Route::get('/cashier', 'Cashier\CashierController@index');
+    Route::get('/cashier/getMenuByCategory/{category_id}', 'Cashier\CashierController@getMenuByCategory');
+    Route::get('/cashier/getTable', 'Cashier\CashierController@getTables');
+    Route::get('/cashier/getSaleDetailsByTable/{table_id}', 'Cashier\CashierController@getSaleDetailsByTable');
+    
+    Route::post('/cashier/orderFood', 'Cashier\CashierController@orderFood');
+    Route::post('/cashier/deleteSaleDetail', 'Cashier\CashierController@deleteSaleDetail');
+    
+    Route::post('/cashier/confirmOrderStatus', 'Cashier\CashierController@confirmOrderStatus');
+    Route::post('/cashier/savePayment', 'Cashier\CashierController@savePayment');
+    Route::get('/cashier/showReceipt/{saleID}', 'Cashier\CashierController@showReceipt');
+    
 });
 
-Route::get('/cashier', 'Cashier\CashierController@index');
-Route::get('/cashier/getMenuByCategory/{category_id}', 'Cashier\CashierController@getMenuByCategory');
-Route::get('/cashier/getTable', 'Cashier\CashierController@getTables');
-Route::get('/cashier/getSaleDetailsByTable/{table_id}', 'Cashier\CashierController@getSaleDetailsByTable');
+Route::middleware(['auth', 'VerifyAdmin'])->group(function(){
+    Route::get('/management', function(){
+        return view('management.index');
+    });
 
-Route::post('/cashier/orderFood', 'Cashier\CashierController@orderFood');
-Route::post('/cashier/deleteSaleDetail', 'Cashier\CashierController@deleteSaleDetail');
+    //routes for management
+    Route::resource('management/category','Management\CategoryController');
+    Route::resource('management/menu','Management\MenuController');
+    Route::resource('management/table','Management\TableController');
 
-Route::post('/cashier/confirmOrderStatus', 'Cashier\CashierController@confirmOrderStatus');
-Route::post('/cashier/savePayment', 'Cashier\CashierController@savePayment');
-Route::get('/cashier/showReceipt/{saleID}', 'Cashier\CashierController@showReceipt');
-
-Route::resource('management/category', 'Management\CategoryController');
-
-Route::resource('management/menu', 'Management\MenuController');
-
-Route::resource('management/table', 'Management\TableController');
-
-//routes for report
-Route::get('/report', 'Report\ReportController@index');
-Route::get('/report/show', 'Report\ReportController@show');
-
-// Export to excel
-Route::get('/report/show/export', 'Report\ReportController@export');
+    //routes for report
+    Route::get('/report', 'Report\ReportController@index');
+    Route::get('/report/show', 'Report\ReportController@show');
+    
+    // Export to excel
+    Route::get('/report/show/export', 'Report\ReportController@export');
+});
